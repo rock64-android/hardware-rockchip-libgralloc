b3f6f9d8e986d1509ec86402acc62cbf28ed72e6
huangds <hds@rock-chips.com>
2014-05-30 11:56:43 +0800

    Revert "add type ,SYSTEM_MASK or VMALLOC"
    This reverts commit a92a3f8f2ba0851496bffe921a6792bb1d09ad26.

--- gralloc_priv.h
+++ gralloc_priv.h
@@ -159,7 +159,7 @@ struct private_handle_t
 	int     lockState;
 	int     writeOwner;
 	int     pid;
-    int     type;
+
 	mali_gralloc_yuv_info yuv_info;
 
 	// Following members is for framebuffer only
@@ -191,7 +191,7 @@ struct private_handle_t
 	 * number of integers that are conditionally included. Similar considerations apply
 	 * to the number of fds.
 	 */
-	static const int sNumInts = 16 + GRALLOC_ARM_NUM_INTS;
+	static const int sNumInts = 15 + GRALLOC_ARM_NUM_INTS;
 	static const int sNumFds = GRALLOC_ARM_NUM_FDS;
 	static const int sMagic = 0x3141592;
 
