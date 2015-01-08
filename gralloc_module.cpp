0fe1e20a9adee66cde13366acc9a6bb034ade2cb
chenzhen <chenzhen@rock-chips.com>
2015-01-08 16:01:05 +0800

    set RK_GRALLOC_VERSION to 1.0.1 for ru_facility.

--- gralloc_module.cpp
+++ gralloc_module.cpp
@@ -45,7 +45,8 @@
 
 #define RK_FBIOGET_IOMMU_STA        0x4632
 
-#define RK_GRALLOC_VERSION "1.001"
+#define RK_GRALLOC_VERSION "1.0.1"
+
 static pthread_mutex_t s_map_lock = PTHREAD_MUTEX_INITIALIZER;
 int g_MMU_stat = 0;
 
